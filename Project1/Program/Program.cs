e27e2960b3f93ad30ecff6f9a1a200e4a6b1195f
Alexan-Alexandrov <a.alexandrov@carx-tech.com>
2025-03-18 14:57:36 +0300

    First_Commit

---
+++ Project1/Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b1;
+            b1 = 2;
+
+            byte b2;
+            sbyte sb;
+            short s;
+            ushort us;
+            int i;
+            uint ui;
+            long l;
+            ulong ul;
+
+            float f = 6.5f;
+            double d = 5.1;
+
+            char c = '3';
+
+            string str = "Hello World";
+
+            bool bl;
+        }
+    }
+}
