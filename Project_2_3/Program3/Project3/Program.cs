a47bd593965dce388edf22476fd93976d00e119b
Alexan-Alexandrov <a.alexandrov@carx-tech.com>
2025-03-18 16:33:28 +0300

    Commit_Unit_2.3

---
+++ Project_2_3/Program3/Project3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //создание переменных
+            short s = 0;
+            int i = 0;
+            long l = 0;
+            float f = 0;
+            double d = 15;
+            object o = d;
+
+            //переобразование типов переменных
+            double d2 = (double)o;
+            i = (int)l; l = i;
+            ushort a = 15050;
+            byte b = (byte)a;
+
+            TestFunc(ref i); // вызов функции
+
+            void TestFunc(ref int value) //передается ссылка на переменную, которую мы изменяем и выводим в консоль
+            {
+                value = 5;
+                Console.WriteLine(i);
+            }
+
+            if (o != null) // проверка не пустая-ли переменная типа object, которая ранее была превращена в double
+            {
+                int hello = Convert.ToInt32(o); // конвертация Double переменной в Int и присваивание значения переменной hello
+                Console.WriteLine("  " + hello);
+            }
+
+            
+            int[] array = new int[10000]; // обычный массив, который не может изменяться по размеру
+
+            List<int> list = new List<int>(80); //динамический массив, который может меняться по размеру
+
+            
+        }
+    }
+}
